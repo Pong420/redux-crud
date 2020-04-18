d71d20a7d25567d2e178629047161fef0267914a
Pong <samfunghp@gmail.com>
2020-04-18 17:15:53 +0800

    fix: upgrade package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: ['plugin:@typescript-eslint/recommended'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
   plugins: ['react-hooks', '@typescript-eslint'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-empty-interface': 0,
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
